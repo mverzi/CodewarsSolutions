fc7d73034f200c9a6669a69bf45beb8efd0e45fb
mverzi <maggie.verzi@gmail.com>
2023-01-07 11:36:49 -0500

    First commit

---
+++ youreASquare.js
@@ -0,0 +1,8 @@
+/**
+ * This function returns if a number is a perfect square or not
+ * @param {int} n 
+ * @returns boolean
+ */
+var isSquare = function(n){
+    return Math.sqrt(n) % 1 == 0
+  }
