c102708d9fcad150207b74ce3a2099cbc7b7231f
mverzi <maggie.verzi@gmail.com>
2023-01-23 17:28:48 -0500

    First commit

---
+++ stringEndsWith.js
@@ -0,0 +1,9 @@
+/**
+ * This function checks to see if the endings match
+ * @param {string} str 
+ * @param {string} ending 
+ * @returns 
+ */
+function solution(str, ending){
+    return str.endsWith(ending)
+  }
