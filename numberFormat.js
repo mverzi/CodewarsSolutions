219cfe3c5672f95bc7856811ce1d591759341e86
mverzi <maggie.verzi@gmail.com>
2023-05-15 11:25:26 -0400

    First commit

---
+++ numberFormat.js
@@ -0,0 +1,8 @@
+/**
+ * This function takes a number and adds any necessary commas
+ * @param {number} number 
+ * @returns string
+ */
+var numberFormat = function (number) {
+    return number.toLocaleString()
+  };
