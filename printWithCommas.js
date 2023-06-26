bd4d0614236efe429ab9ae06a3a98e1e877af8f5
mverzi <maggie.verzi@gmail.com>
2023-06-26 15:11:56 -0400

    First commit

---
+++ printWithCommas.js
@@ -0,0 +1,8 @@
+/**
+ * This function takes an array of strings and returns them as a comma separated string
+ * @param {str[]} array 
+ * @returns string
+ */
+function printArray(array){
+    return array.join(',')
+  }
