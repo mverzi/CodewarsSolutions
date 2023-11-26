debd8228f61151a6a70987c3faa8292b30cc6f66
mverzi <maggie.verzi@gmail.com>
2023-11-26 15:47:34 -0500

    First commit

---
+++ totalAmountOfPoints.js
@@ -0,0 +1,12 @@
+function points(games) {
+    let points = 0;
+    for(let i = 0; i < games.length; i++){
+      if(games[i][0] > games[i][2]){
+        points += 3;
+      }
+      else if(games[i][0] === games[i][2]){
+        points += 1;
+      }
+    }
+    return points;
+  }
