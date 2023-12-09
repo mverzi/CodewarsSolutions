434088de58413785f941722acb17e270b7aa5d71
mverzi <maggie.verzi@gmail.com>
2023-12-09 09:28:50 -0500

    First commit

---
+++ welcome.js
@@ -0,0 +1,24 @@
+function greet(language) {
+    let greetings = [
+      ["english", "Welcome"],
+      ["czech", "Vitejte"],
+      ["danish", "Velkomst"],
+      ["dutch", "Welkom"],
+      ["estonian", "Tere tulemast"],
+      ["finnish", "Tervetuloa"],
+      ["flemish", "Welgekomen"],
+      ["french", "Bienvenue"],
+      ["german", "Willkommen"],
+      ["irish", "Failte"],
+      ["italian", "Benvenuto"],
+      ["latvian", "Gaidits"],
+      ["lithuanian", "Laukiamas"],
+      ["polish", "Witamy"],
+      ["spanish", "Bienvenido"],
+      ["swedish", "Valkommen"],
+      ["welsh", "Croeso"]
+    ];
+    
+      let index = greetings.findIndex(pair => pair[0] === language)
+      return index > 0 ? greetings[index][1] : "Welcome"
+    }
