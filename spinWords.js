58f2995101a4fdcd6fa52a691a1abe5c06f5b45d
mverzi <maggie.verzi@gmail.com>
2024-01-08 17:56:08 -0500

    First commit

---
+++ spinWords.js
@@ -0,0 +1,9 @@
+function spinWords(string){
+    return string.split(' ').map(word => {
+          if(word.length >= 5){
+              return word.split('').reverse().join('')
+          } else {
+              return word
+          }
+      }).join(' ')
+  }
