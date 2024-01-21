4ac3acf7afdb4871108b7ca22e9175710b0fafe1
mverzi <maggie.verzi@gmail.com>
2024-01-21 10:06:05 -0500

    First commit

---
+++ appleTurnover.js
@@ -0,0 +1,8 @@
+/**
+ * This function returns a phrase based on the value of x^2
+ * @param {integer} x 
+ * @returns a string
+ */
+function apple(x){
+    return x**2 > 1000 ? "It's hotter than the sun!!" : "Help yourself to a honeycomb Yorkie for the glovebox."
+  }
