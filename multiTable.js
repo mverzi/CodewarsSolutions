88bd12e5f1701a1ec2ef1ef3e64f1116ab757b2a
mverzi <maggie.verzi@gmail.com>
2024-01-26 11:42:31 -0500

    First commit

---
+++ multiTable.js
@@ -0,0 +1,9 @@
+function multiTable(number) {
+    let result = ''
+    
+    for(let i = 1; i <= 10; i++) {
+         i != 10 ? result += `${i} * ${number} = ${i * number}\n` :  result += `${i} * ${number} = ${i * number}`
+    }
+    
+    return result
+  }
